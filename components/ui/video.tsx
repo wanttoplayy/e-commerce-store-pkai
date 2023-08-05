0498b7417236de3dfb7e0a01a16d4900f35f7f63
Vasin Suksuchit <vasinsuk@gosoft.co.th>
2023-08-05 19:31:12 +0700

    fix:video able to play

--- components/ui/video.tsx
+++ components/ui/video.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const VideoPlayer = () => {
-    return (
-        <div className='flex flex-col items-center gap-4 '>
-            <h2>วีดีโอสินค้าของเรา</h2>
-            <video controls className='w-[700px]'>
-                <source src="@/public/introduce.mp4" type=' mp4' />
-            </video>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center gap-4">
+      <h2>วีดีโอสินค้าของเรา</h2>
+
+      {/* Replace <video> with YouTube iframe */}
+      <iframe
+        width="700"
+        height="394"
+        src="https://www.youtube.com/embed/alE3S1nTCIc"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default VideoPlayer;
